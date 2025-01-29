608d7a341a6e7b25c3de1f27137d0d127d1f2cd3
mithridatem <mathieumithridate@adrar-formation.com>
2025-01-29 14:05:02 +0100

    feat : php/documentor model/category.php

--- model/category.php
+++ model/category.php
@@ -1,14 +1,18 @@
 <?php
 
-
-function addCategory($bdd, $name){
+/**
+ * @param PDO $bdd
+ * @param string $name 
+ */
+function addCategory($bdd, $name)
+{
     //Requête
     $requete = "INSERT INTO category(name) VALUE(?)";
     try {
         //Préparation de la requête
         $req = $bdd->prepare($requete);
         //Associer les paramètres (?)
-        $req->bindParam(1,$name, PDO::PARAM_STR);
+        $req->bindParam(1, $name, PDO::PARAM_STR);
         //Exécuter la requête
         $req->execute();
     } catch (Exception $e) {
@@ -16,15 +20,22 @@ function addCategory($bdd, $name){
     }
 }
 
-function aupdateCategory($bdd, $name, $id){
+/**
+ * @param PDO $bdd 
+ * @param string $name 
+ * @param int $id
+ * @return void
+ */
+function updateCategory($bdd, $name, $id): void
+{
     //Requête
     $requete = "UPDATE category SET name=? WHERE id_category=?";
     try {
         //Préparation de la requête
         $req = $bdd->prepare($requete);
         //Associer les paramètres (?)
-        $req->bindParam(1,$name, PDO::PARAM_STR);
-        $req->bindParam(2,$id, PDO::PARAM_INT);
+        $req->bindParam(1, $name, PDO::PARAM_STR);
+        $req->bindParam(2, $id, PDO::PARAM_INT);
         //Exécuter la requête
         $req->execute();
     } catch (Exception $e) {
@@ -32,14 +43,21 @@ function aupdateCategory($bdd, $name, $id){
     }
 }
 
-function deleteCategory($bdd, $name, $id){
+
+/**
+ * @param PDO $bdd 
+ * @param int $id
+ * @return void
+ */
+function deleteCategory($bdd, $id): void
+{
     //Requête
     $requete = "DELETE FROM category WHERE id_category = ?";
     try {
         //Préparation de la requête
         $req = $bdd->prepare($requete);
         //Associer les paramètres (?)
-        $req->bindParam(1,$id, PDO::PARAM_INT);
+        $req->bindParam(1, $id, PDO::PARAM_INT);
         //Exécuter la requête
         $req->execute();
     } catch (Exception $e) {
@@ -47,14 +65,20 @@ function deleteCategory($bdd, $name, $id){
     }
 }
 
-function getCategoryByName($bdd, $name){
+/**
+ * @param PDO $bdd 
+ * @param string $name
+ * @return array|null
+ */
+function getCategoryByName($bdd, $name): array|null
+{
     //Requête
     $requete = "SELECT id_category, name FROM category WHERE name=?";
     try {
         //Préparation de la requête
         $req = $bdd->prepare($requete);
         //Associer les paramètres (?)
-        $req->bindParam(1,$name, PDO::PARAM_STR);
+        $req->bindParam(1, $name, PDO::PARAM_STR);
         //Exécuter la requête
         $req->execute();
         $data = $req->fetch(PDO::FETCH_ASSOC);
@@ -64,7 +88,12 @@ function getCategoryByName($bdd, $name){
     }
 }
 
-function getAllCategory($bdd){
+/**
+ * @param PDO $bdd 
+ * @return array|null
+ */
+function getAllCategory($bdd): array|null
+{
     //Requête
     $requete = "SELECT id_category, name FROM category";
     try {
