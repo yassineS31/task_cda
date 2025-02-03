2a0f282f5bebed9d59ffb98437831e41c3af3e65
yassine <y31m3x@gmail.com>
2025-02-03 14:21:04 +0100

    maj

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include './env.php';
 include './controller/accountController.php';
 include './utils/connexion.php';
 include './vue/header.php';
+
 // include 'controller/categorieController.php';
 include './vue/footer.php';
 $bdd = connexion();
